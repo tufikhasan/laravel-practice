303f00d636c0dc266ef8e1a4e87596bf9c5edfc1
tufik <tufikhasan05@gmail.com>
2023-02-06 23:23:18 +0600

    Write a program to find even or odd number - (both negative or positive)

--- index.php
+++ index.php
@@ -1,22 +1,65 @@
 <?php
-/* How to find Leap year  */
+/* Write a program to find even or odd number - (both negative or positive)  */
 
-$year = 1900;
-
-//solution 01:
-if ( $year % 4 == 0 && $year % 100 == 0 && $year % 400 == 0 ) {
-    echo "{$year} is a leap year\n";
-} elseif ( $year % 4 == 0 && $year % 100 == 0 ) {
-    echo "{$year} is not a leap year\n";
-} elseif ( $year % 4 == 0 ) {
-    echo "{$year} is a leap year\n";
-} else {
-    echo "{$year} is not a leap year\n";
+//Solution 01: nested
+$num = -23;
+$r = $num % 2;
+switch ( $r ) {
+case 0:
+    switch ( $num ) {
+    case $num > 0:
+        echo "{$num} Positive Even number\n";
+        break;
+    case $num < 0:
+        echo "{$num} Negative Even number\n";
+        break;
+    }
+    break;
+default:
+    switch ( $num ) {
+    case $num > 0:
+        echo "{$num} Positive Odd number\n";
+        break;
+    case $num < 0:
+        echo "{$num} Negative Odd number\n";
+        break;
+    }
 }
 
-//solution 02:
-if ( $year % 4 == 0 && ( $year % 100 != 0 || $year % 400 == 0 ) ) {
-    echo "{$year} is a leap year\n";
-} else {
-    echo "{$year} is not a leap year\n";
+//Solution 02:
+$num = -3;
+$r = $num % 2;
+switch ( true ) {
+case ( 0 == $r && $num > 0 ):
+    echo "$num is a positive even number";
+    break;
+case ( 1 == $r && $num > 0 ):
+    echo "$num is a positive odd number";
+    break;
+case ( 0 == $r && $num < 0 ):
+    echo "$num is a negative even number";
+    break;
+case ( -1 == $r && $num < 0 ):
+    echo "$num is a negative odd number";
+    break;
+}
+
+echo "\n";
+
+//Solution 03:
+$num = -3;
+$r = abs( $num ) % 2;
+switch ( true ) {
+case ( 0 == $r && $num > 0 ):
+    echo "$num is a positive even number";
+    break;
+case ( 1 == $r && $num > 0 ):
+    echo "$num is a positive odd number";
+    break;
+case ( 0 == $r && $num < 0 ):
+    echo "$num is a negative even number";
+    break;
+case ( 1 == $r && $num < 0 ):
+    echo "$num is a negative odd number";
+    break;
 }
