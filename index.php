8b21b1eedc2fba37d7b1332a031048c1827577b5
tufik <tufikhasan05@gmail.com>
2023-02-06 22:18:55 +0600

    Write a program to check student grade based on the marks

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
-/** Module 2 Assignment:
- * HF consultancy wants to build a very simple commission-calculating calculator for their Admission agents. Usually, the commission is twenty-five percent of the tuition fee if the tuition is above twenty thousand dollars. But if the tuition fee is above ten thousand dollars but less than twenty thousand dollars, the commission is twenty percent. If the tuition fee is less than ten thousand dollars but greater than seven thousand dollars,  the commission rate is fifteen percent. If the tuition fee is below seven thousand dollars the data will be invalid. As a developer please help HF Consultancy for building this simple calculator using a ternary operator in Php.
- */
-$tuitionFee = 20000;
-$commission = ( $tuitionFee >= 20000 ) ? ( "Commission 25% = " . $tuitionFee * 25 / 100 ) : (  ( $tuitionFee >= 10000 && $tuitionFee < 20000 ) ? ( "Commission 20% = " . $tuitionFee * 20 / 100 ) : (  ( $tuitionFee >= 7000 && $tuitionFee < 10000 ) ? ( "Commission 15% = " . $tuitionFee * 15 / 100 ) : "Invalid tuition fee" ) );
+/* Write a program to check student grade based on the marks using if-else statement.
 
-echo "$commission";
+Conditions:
+-> If marks are 60% or more, grade will be First Division.
+-> If marks between 45% to 59%, grade will be Second Division.
+-> If marks between 33% to 44%, grade will be Third Division.
+-> If marks are less than 33%, student will be Fail.
+ */
+$marks = 60;
+
+//solution using - if...elseif...else
+if ( $marks >= 60 && $marks <= 100 ) {
+    echo "First Division\n";
+} elseif ( $marks >= 45 && $marks < 60 ) {
+    echo "Second Division\n";
+} elseif ( $marks >= 33 && $marks < 45 ) {
+    echo "Third Division\n";
+} elseif ( $marks > 0 && $marks < 33 ) {
+    echo "Fail\n";
+} else {
+    echo "Invalid marks\n";
+}
+
+//solution using ternary operator:
+$result = ( $marks >= 80 && $marks <= 100 ) ? "A+" : (  ( $marks >= 70 && $marks < 80 ) ? "A" : (  ( $marks < 33 && $marks > 0 ) ? "Fail" : (  ( $marks >= 60 && $marks < 70 ) ? "A-" : (  ( $marks >= 50 && $marks < 60 ) ? "B" : (  ( $marks >= 40 && $marks < 50 ) ? "C" : (  ( $marks >= 33 && $marks < 40 ) ? "D" : "invalid marks" ) ) ) ) ) );
+echo $result;
