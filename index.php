f8b58ff16254c1434ffc404b01d3006147e62d50
tufik <tufikhasan05@gmail.com>
2023-02-06 22:46:52 +0600

    Write a program to check whether the given number is positive or negative using  ternary operator

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
-/* write a program to swap the values of two variables. */
+/* Write a program to check whether the given number is positive or negative using  ternary operator */
 
 //solution 01:
-$num1 = 26;
-$num2 = 72;
-$num1 += $num2; // 26 + 72 = 98; now num1 = 98;
-$num2 = $num1 - $num2; // 98 - 72 = 26; now num2 = 26
-$num1 -= $num2; // 98 - 26 = 72; now num1 = 72
-echo "Now num1 = $num1, num2 = $num2 \n";
-
-//solution 02:
-$number1 = 1;
-$number2 = 2;
-
-$temp = $number1;
-$number1 = $number2;
-$number2 = $temp;
-echo "Now number1 = $number1, number2 = $number2";
+$number = 4;
+echo ( $number > 0 ) ? "$number is a positive number" : (  ( 0 == $number ) ? "Zero is neither positive or negative number" : "$number is a negative number" );
