bfbb23b8b1c1748f89a656bb05c949223134376b
tufik <tufikhasan05@gmail.com>
2023-02-06 22:59:18 +0600

    Write a program to Find large number using switch case

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
-/* Write a program to check whether the given number is positive or negative using  ternary operator */
+/* Find large number using switch case */
 
 //solution 01:
-$number = 4;
-echo ( $number > 0 ) ? "$number is a positive number" : (  ( 0 == $number ) ? "Zero is neither positive or negative number" : "$number is a negative number" );
+$number1 = 60;
+$number2 = 40;
+$number3 = 60;
+
+switch ( true ) {
+case ( $number1 == $number2 && $number1 == $number3 ):
+    echo "All number are equal";
+    break;
+case ( $number1 >= $number2 && $number1 >= $number3 ):
+    echo "The larger number is $number1";
+    break;
+case ( $number2 >= $number3 ):
+    echo "The larger number is $number2";
+    break;
+default:
+    echo "The larger number is $number3";
+}
