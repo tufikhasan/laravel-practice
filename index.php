0fefea4d5f0c47e0173e08369b071f89b3fbfefa
tufik <tufikhasan05@gmail.com>
2023-02-12 23:59:24 +0600

    Write a function that takes an array of integers & a target sum as input. if sum= target return true

--- index.php
+++ index.php
@@ -1,65 +1,23 @@
 <?php
-/* Write a program to find even or odd number - (both negative or positive)  */
+/*
+Problem: Write a function that takes an array of integers and a target sum as input , and returns a boolean indicating whether there exists a pair of integers in the array that add up to the target sum. You can assume the array has at least two elements and all elements are unique. use a loop in your solution.
 
-//Solution 01: nested
-$num = -23;
-$r = $num % 2;
-switch ( $r ) {
-case 0:
-    switch ( $num ) {
-    case $num > 0:
-        echo "{$num} Positive Even number\n";
-        break;
-    case $num < 0:
-        echo "{$num} Negative Even number\n";
-        break;
+input: [5,7,1,2,8,4,3],8
+output: true(because 5+3)8
+ */
+function hasPairWithSum( $arr, $target_sum ) {
+    for ( $i = 0; $i < count( $arr ); $i++ ) {
+        for ( $j = $i + 1; $j < count( $arr ); $j++ ) {
+            if ( $arr[$i] + $arr[$j] == $target_sum ) {
+                return true;
+            }
+        }
     }
-    break;
-default:
-    switch ( $num ) {
-    case $num > 0:
-        echo "{$num} Positive Odd number\n";
-        break;
-    case $num < 0:
-        echo "{$num} Negative Odd number\n";
-        break;
-    }
+    return false;
 }
-
-//Solution 02:
-$num = -3;
-$r = $num % 2;
-switch ( true ) {
-case ( 0 == $r && $num > 0 ):
-    echo "$num is a positive even number";
-    break;
-case ( 1 == $r && $num > 0 ):
-    echo "$num is a positive odd number";
-    break;
-case ( 0 == $r && $num < 0 ):
-    echo "$num is a negative even number";
-    break;
-case ( -1 == $r && $num < 0 ):
-    echo "$num is a negative odd number";
-    break;
-}
-
-echo "\n";
-
-//Solution 03:
-$num = -3;
-$r = abs( $num ) % 2;
-switch ( true ) {
-case ( 0 == $r && $num > 0 ):
-    echo "$num is a positive even number";
-    break;
-case ( 1 == $r && $num > 0 ):
-    echo "$num is a positive odd number";
-    break;
-case ( 0 == $r && $num < 0 ):
-    echo "$num is a negative even number";
-    break;
-case ( 1 == $r && $num < 0 ):
-    echo "$num is a negative odd number";
-    break;
+$result = hasPairWithSum( [5, 7, 1, 2, 8, 4, 3], 8 );
+if ( $result ) {
+    echo "True";
+} else {
+    echo "False";
 }
