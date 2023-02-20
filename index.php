9272a8d8cf3822a0353a041a1e110475a4e117c5
tufik <tufikhasan05@gmail.com>
2023-02-20 15:14:52 +0600

    Find biggest word in a string

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <?php
-/*********** Multiplication Table */
 
-function multiplicationTable( int $num ) {
-    for ( $i = 1; $i <= 10; $i++ ) {
-        echo "$i x $num = " . $i * $num . PHP_EOL;
+/*********** Find biggest word in a string */
+
+//01:
+function biggestWord( $str ) {
+    $words = explode( " ", $str );
+    $bigWord = "";
+    foreach ( $words as $word ) {
+        if ( strlen( $word ) > strlen( $bigWord ) ) {
+            $bigWord = $word;
+        }
     }
+    return $bigWord;
 }
-multiplicationTable( 3 );
+$str = "The quick brown fox jumped over the lazy dog";
+echo biggestWord( $str );
+
+// //02:
+// function longestWord( $str ) {
+//     $str = preg_replace( '/[^A-Za-z0-9 ]/', '', $str );
+
+//     $words = explode( ' ', $str );
+
+//     $longest_word = '';
+
+//     foreach ( $words as $word ) {
+//         if ( strlen( $word ) > strlen( $longest_word ) ) {
+//             $longest_word = $word;
+//         }
+//     }
+//     return $longest_word;
+// }
+// $str = "The quick brown fox jumped over the lazy dog";
+// echo longestWord( $str );
