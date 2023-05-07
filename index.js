2e8259ce69a73ed7708da8ff6b37aa062da73f32
tufik <tufikhasan05@gmail.com>
2023-05-07 16:06:12 +0600

    tailwind suggestion setup

--- index.js
+++
@@ -1,8 +0,0 @@
-//Assignment 11 - Multiplication table
-function multiplicationTable(num) {
-  for (let i = 1; i <= 10; i++) {
-    const product = num * i;
-    console.log(num + ' x ' + i + ' = ' + product);
-  }
-}
-multiplicationTable(5);
