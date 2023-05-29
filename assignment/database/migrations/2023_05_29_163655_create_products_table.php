5ced72c58afa64faaa1721016974cf1861a3ebd6
Towfik Hasan <tufikhasan05@gmail.com>
2023-05-29 22:45:10 +0600

    ....

--- assignment/database/migrations/2023_05_29_163655_create_products_table.php
+++ assignment/database/migrations/2023_05_29_163655_create_products_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
+    public function up(): void{
+        Schema::create( 'products', function ( Blueprint $table ) {
             $table->id();
+            $table->string( 'title' );
+            $table->integer( 'price' );
             $table->timestamps();
-        });
+        } );
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
+    public function down(): void{
+        Schema::dropIfExists( 'products' );
     }
 };
