e5b3534db81eda4b4622207437703718c9b646a8
Towfik Hasan <tufikhasan05@gmail.com>
2023-08-17 14:33:50 +0600

    Module 24 assignment completed

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller {
 
     function reservation( Request $request ) {
         $request->validate( [
-            'email'  => 'required|unique:reservations,email',
-            'mobile' => 'required|unique:reservations,mobile',
+            'email'  => 'required',
+            'mobile' => 'required',
         ] );
+        $exists = Reservation::where( 'event_id', $request->event_id )
+            ->where( function ( Builder $query ) use ( $request ) {
+                $query->where( 'mobile', $request->mobile )
+                    ->orWhere( 'email', $request->email );
+            } )->first();
+        if ( $exists == 1 ) {
+            $notification = [
+                'message'    => "You Are Already Reserved",
+                'alert-type' => 'success',
+            ];
+            return redirect()->back()->with( $notification );
+        }
         Reservation::create( [
             'event_id' => $request->event_id,
             'name'     => $request->name,
